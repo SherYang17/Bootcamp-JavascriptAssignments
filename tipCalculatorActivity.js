e96c92cd8c287224f42e6e22c4af0905301af13e
SherYang17 <127764182+SherYang17@users.noreply.github.com>
2023-04-06 11:39:23 -0400

    Changed some wording and corrected some mispelling

--- tipCalculatorActivity.js
+++ tipCalculatorActivity.js
@@ -2,8 +2,8 @@
 let totalAmount = 200;
 
 // Create a function named 'calculateTip'
-function calculateTip(amount) {  // - Takes in an amount as an argument, then later, when calling the function, use the variable (totalAmount) from above.
-  return amount * 0.2;           // ( It should multiply the bill variable by 20% of the total and return that value)
+function calculateTip(amount) {  // - Take in an amount as an argument, then later, when calling the function, use the variable (totalAmount) from above
+  return amount * 0.2;           // - It should multiply the bill variable by 20% of the total and return that value
 }
 
 // Create another function called 'getBillTotal' and it should do the following things:
@@ -16,7 +16,7 @@ function getBillTotal(amount) { // - Take any amount as an argument
 console.log("Tip: $" + calculateTip(totalAmount));
 console.log("Total: $" + getBillTotal(totalAmount));
 
-// Bonus, enable the 2 digits
+// Bonus, round your amount returned to 2 decimal places.
 let tip = calculateTip(totalAmount);
 let twoDigits = tip.toFixed(2);      //https://linuxhint.com/round-number-to-2-decimal-places-javascript/
 console.log("Total with 2 digits:$ " + twoDigits);
