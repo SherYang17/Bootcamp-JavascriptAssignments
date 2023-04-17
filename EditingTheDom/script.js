c3ab6373d50cf361c96518ce98fe958eb0360701
yangs15000@gmail.com <yangs15000@gmail.com>
2023-04-17 15:03:47 -0400

    making changes

---
+++ EditingTheDom/script.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function () {
+// Update the 'Apples' item to say 'Granny Smith Apples'
+document.querySelector("#list li:nth-child(2)").innerText = "Granny Smith Apples"; //select the 2nd from list and innerText to rewrite it
+
+// Remove 'Oat Milk' from the list
+document.querySelector("#list li:nth-child(4)").remove(); //simply select the 4th from list and remove
+
+// // Add an item 'Kombucha'
+const kombuchaItem = document.createElement("li"); // const variable = create me a li
+kombuchaItem.innerText = "Kombucha"; // variable text = kombucha
+document.getElementById("list").appendChild(kombuchaItem); // get list id and append to kombuchaitem
+
+
+// // Clear the list and programmatically add items from the array ['protein bars', 'almonds', 'peanut butter']
+const newItems = ["protein bars", "almonds", "peanut butter"]; //create array
+const list = document.getElementById("list"); // define the elementid list to a variable
+list.innerHTML = ""; // make the variable blank with no text between quotes
+
+// // Use for loop to add in the array, loops thru each item and creates list item and append at the end
+for (let item of newItems) { //for each item inside newItems array created above
+  const listItem = document.createElement("li"); //create a variable to define the creation of li's
+  listItem.innerText = item; //each item looping thru newitems will write the text 
+  list.appendChild(listItem); //append/rewrite to each listitem that it loops through
+}
+
+// // Add the class 'important' to the almonds item.
+ document.querySelector("#list li:nth-child(2)").classList.add("important"); //element.classList.add("newClass");, just mounting it on
+});
+
+
+
+
+
+
+
+
+
+
+//https://bobbyhadz.com/blog/javascript-get-nth-child-of-element
+// loop related document https://gomakethings.com/two-more-ways-to-create-html-from-an-array-of-data-with-vanilla-js/
+
+
+
+
+
+
+
+// 8// const kombuchaItem = document.createElement("li");
+// kombuchaItem.innerText = "Kombucha";
+// document.getElementById("list").appendChild(kombuchaItem);
---
+++ Traversing The Dom/script.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", function() {
+// 1. Get the header element
+const header = document.querySelector("header");
+console.log("This is the Header: ", header);
+
+// 2. Get all the section elements
+const sectionElements = document.querySelectorAll("section");
+console.log("This is the sectionElements: ", sectionElements);
+
+// 3. Get the section element with class="current"
+const currentSection = document.querySelector("section.current");
+console.log("This is the currentSection:", currentSection);
+
+// 4. Get the section that comes after the current section
+const nextSection = currentSection.nextElementSibling;
+console.log("This is the nextSection:", nextSection);
+
+// 5. Get the h2 node from the section before the 'current' section
+const previousSection = currentSection.previousElementSibling;
+const h2Node = previousSection.querySelector("h2");
+console.log("This is the h2Node: ", h2Node);
+// const h2BeforeCurrent = document.querySelector('.current').previousElementSibling.querySelector('h2');
+// console.log("This is the h2Node Number2: ", h2BeforeCurrent);
+
+// 6. Get the div that contains the section that has an h2 with a class of 'highlight'
+const divWithHighlight = document.querySelector("div.col h2.highlight").parentNode.parentNode;
+//const divWithHighlight = document.querySelector('h2.highlight').parentNode.parentNode;
+console.log("This is the divContainingHighlight: ", divWithHighlight);
+
+// 7. Get all the sections that contain an H2 (using a single statement)
+const sectionsContainingH2 = document.querySelectorAll("section:has(h2)");
+  console.log("This is the sectionsContainingH2: ", sectionsContainingH2);
+});
+
+
+
+
+//https://javascript.info/onload-ondomcontentloaded
+//https://javascript.info/onload-ondomcontentloaded
+//https://stackoverflow.com/questions/46744616/uncaught-typeerror-cannot-read-property-nextsibling-of-undefined
+//https://www.w3schools.com/jsref/met_document_addeventlistener.asp
+//https://www.javascripttutorial.net/javascript-dom/javascript-domcontentloaded/
+//https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+//RIDICULOUS
+
+
+//document.addEventListener('DOMContentLoaded', () => {
+//OR
+//document.addEventListener("click", function(){
+//     document.getElementById("demo").innerHTML = "Hello World";
+// });
+//previous siblings
+//https://www.w3schools.com/jsref/prop_element_previouselementsibling.asp
+// wowwwwwwwwwwwwwwwwwwwwwwwww it didn't work because I didnt have the event listener. Works 100% with listener. 
+
+
+
+
+//document at the dom, dealing with browsers, node is like I dont know what that is. That's why there is error ReferenceError document is not defined. Run code on chrome >:).
