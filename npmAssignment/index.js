92b8b6a8c6f0ca6db47df7286f40e8eee2559ece
yangs15000@gmail.com <yangs15000@gmail.com>
2023-04-25 12:39:57 -0400

    making additional changes

--- npmAssignment/index.js
+++ npmAssignment/index.js
@@ -1,10 +1,10 @@
-const moment = require('moment');
+// const moment = require('moment');
 
-console.log(moment().format('h:mm:ss a'));
+// console.log(moment().format('h:mm:ss a'));
 
 //const myMod = require('./myModule');
 //myMod.sayHi();
 
-// const Moment = require('moment');
-// const a = new Moment();
-// console.log(a.format('h:mm:ss a')); //logs the current time
+const Moment = require('moment');
+const a = new Moment();
+console.log(a.format('h:mm:ss a')); //logs the current time
