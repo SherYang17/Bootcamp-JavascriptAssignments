31dd6f57364c0de645cef3d5475e3641ca52c072
yangs15000@gmail.com <yangs15000@gmail.com>
2023-04-25 09:39:38 -0400

    Uploading hw with correct code in index.js file

--- npmAssignment/index.js
+++ npmAssignment/index.js
@@ -1,4 +1,10 @@
-import Moment from 'moment';
+const moment = require('moment');
 
-const a = new Moment();
-console.log(a.format('h:mm:ss a'));
+console.log(moment().format('h:mm:ss a'));
+
+//const myMod = require('./myModule');
+//myMod.sayHi();
+
+// const Moment = require('moment');
+// const a = new Moment();
+// console.log(a.format('h:mm:ss a')); //logs the current time
